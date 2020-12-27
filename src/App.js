572b98df728f3d9b53ef18137659c58541ae23ee
Sami Jones <62959599+sammjones@users.noreply.github.com>
2020-12-27 15:14:13 -0700

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-      </header>
-      <footer>
-        Open-source code on GitHub by{" "}
-        <a
-          href="https://github.com/sammjones/react-final-project-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Sami Jones
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>React Weather App</h1>
+        </header>
+        <footer>
+          Open-source code on GitHub by{" "}
+          <a
+            href="https://github.com/sammjones/react-final-project-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sami Jones
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
